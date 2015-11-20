c71778f449a1549e5a5ef2c98c1e8e8ae43a1b90
Ross Jones <rossj@trackmatic.co.za>
2015-11-20 10:40:11 +0200

    fix cors plugin model

--- Kong.Plugins/Model/CorsPluginConfig.cs
+++ Kong.Plugins/Model/CorsPluginConfig.cs
@@ -1,9 +1,17 @@
-﻿using Newtonsoft.Json;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Kong.Plugins.Model
 {
     public class CorsPluginConfig
     {
+        public CorsPluginConfig()
+        {
+            Methods = new List<string>();
+            Headers = new Dictionary<string, string>();
+            ExposedHeaders = new Dictionary<string, string>();
+        }
+
         [JsonProperty("credentials")]
         public bool Credentials { get; set; }
 
@@ -14,13 +22,13 @@ namespace Kong.Plugins.Model
         public string Origin { get; set; }
 
         [JsonProperty("methods")]
-        public string Methods { get; set; }
+        public List<string> Methods { get; set; }
 
         [JsonProperty("headers")]
-        public string Headers { get; set; }
+        public Dictionary<string, string> Headers { get; set; }
 
         [JsonProperty("exposed_headers")]
-        public string ExposedHeaders { get; set; }
+        public Dictionary<string, string> ExposedHeaders { get; set; }
 
         [JsonProperty("max_age")]
         public long MaxAge { get; set; }
