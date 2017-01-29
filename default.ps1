ab9ac510b06e3a1222dfacfe114ac9fe7210752e
RossJayJones <rossj@trackmatic.co.za>
2017-01-29 21:00:44 +0200

    fix up build script

--- default.ps1
+++ default.ps1
@@ -10,32 +10,8 @@ properties {
 Task default -depends Clean, Restore, Build
 
 Task Publish-Nuget-Package {
-	$spec = [xml](get-content "$base_dir/Kong.NuGet/Kong.nuspec")
-	$version = $spec.package.metadata.version
-	$package = "$base_dir/Kong.$version.nupkg"
-    $package_dir = "$base_dir/Kong.NuGet/bin/package"
-    
-    # Prepare package folder
-    remove-item $package_dir -R -ErrorAction SilentlyContinue
-    new-item -itemtype directory "$package_dir"
-    new-item -itemtype directory "$package_dir/lib"
-    new-item -itemtype directory "$package_dir/lib/net45"
-    
-    # Copy nuspec file to package folder
-    copy-item "$base_dir/Kong.NuGet/Kong.nuspec" "$package_dir/Kong.nuspec"
-
-    # Copy libraries to package folders
-    get-childitem "$base_dir/Kong.NuGet/bin/Debug" | ? { $_.Name -like "Kong*.dll" -and $_.Name } | % { copy-item $_.FullName "$package_dir/lib/net45" }
-
-    # Create nuget package and upload to nuget
-	exec { & $nuget pack "$package_dir/Kong.nuspec" }
     $apikey = Read-Host -Prompt 'Enter Api Key'
-	exec { & $nuget setApiKey $apikey }
-	exec { & $nuget push "$package" }
-
-    # Perform some cleanup on the folder
-	remove-item $package
-    remove-item $package_dir -R -ErrorAction SilentlyContinue
+    publish-package "Kong" $apikey
 }
 
 Task Build {
@@ -54,4 +30,34 @@ Task Restore {
     Get-ChildItem $base_dir -Filter *.sln | % {
         exec { & "$base_dir\.nuget\NuGet.exe" restore "$_"  }
     }
+}
+
+function publish-package($nuspec, $apikey) {
+    $file = "$base_dir\$nuspec\$nuspec.nuspec"
+    write-host $file
+    $spec = [xml](get-content $file)
+    $version = $spec.package.metadata.version
+    $package = "$base_dir\$nuspec.$version.nupkg"
+    $package_dir = "$base_dir\$nuspec\bin\package"
+    
+    # Prepare package folder
+    remove-item $package_dir -R -ErrorAction SilentlyContinue
+    new-item -itemtype directory "$package_dir"
+    new-item -itemtype directory "$package_dir/lib"
+    new-item -itemtype directory "$package_dir/lib/net45"
+    
+    # Copy nuspec file to package folder
+    copy-item "$base_dir\$nuspec\$nuspec.nuspec" "$package_dir\$nuspec.nuspec"
+
+    # Copy libraries to package folders
+    get-childitem "$base_dir\$nuspec\bin\Debug" | ? { $_.Name -like "$nuspec*.dll" -and $_.Name } | % { copy-item $_.FullName "$package_dir\lib\net45" }
+
+    # Create nuget package and upload to nuget
+    exec { & $nuget pack "$package_dir/$nuspec.nuspec" }
+    exec { & $nuget setApiKey $apikey }
+    exec { & $nuget push "$package" }
+
+    # Perform some cleanup on the folder
+    remove-item $package
+    remove-item $package_dir -R -ErrorAction SilentlyContinue
 }
