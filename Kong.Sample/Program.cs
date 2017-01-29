34c832fb7aa5b0b6cc0e4fa7eb11a1f1b31754cb
RossJayJones <rossj@trackmatic.co.za>
2017-01-29 20:54:45 +0200

    update sample

--- Kong.Sample/Program.cs
+++ Kong.Sample/Program.cs
@@ -20,57 +20,11 @@ namespace Kong.Sample
 
             var client = factory.Create();
 
-            var node = client.Node().Result;
+            var node = await client.Node();
 
-            var consumers = client.Consumers.List().Result;
+            var consumers = await client.Consumers.List();
 
-            var apis = client.Apis.List(requestHost: "rest.api.com22").Result;
-
-
-            var api = client.Apis.Get("google").Result;
-
-            var newPlugin = await api.Plugins.Create(new PluginData
-            {
-                Config = new IpRestrictionPlugin
-                {
-                    Blacklist = new [] { "10.10.0.10"}
-                }
-            });
-            
-
-            var plugin = await api.Plugins.Get(newPlugin.Id);
-
-            //await plugin.Delete();
-
-            var configuration = plugin.Configure<AclPlugin>();
-
-            await configuration.Credentials(consumers.Data[0].Id).Create("group1");
-
-            var credentials = await configuration.Credentials(consumers.Data[0].Id).List();
-
-            foreach (var item in credentials.Data)
-            {
-                await configuration.Credentials(consumers.Data[0].Id).Delete(item.Id);
-            }
-
-            //var credential = await credentials.Create("a36c3049b36249a3c9f8891cb127243c", "e71829c351aa4242c2719cbfbe671c09");
-
-            /*((CorsPlugin) cors).Config.ExposedHeaders = new List<string> { "Authorization" };
-
-
-            ((CorsPlugin) cors).Config.Methods = new List<string>
-            {
-                "GET",
-                "HEAD",
-                "PUT",
-                "PATCH",
-                "POST",
-                "DELETE"
-            };
-            ((CorsPlugin) cors).Config.Headers = null;
-            ((CorsPlugin) cors).Config.PreflightContinue = true;
-            await plugins.Put(cors);*/
-
+            var apis = await client.Apis.List();
         }
     }
 }
