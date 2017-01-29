9d563f5d953bc76c83e2e780ed13df738fd97d86
RossJayJones <rossj@trackmatic.co.za>
2017-01-29 20:47:54 +0200

    implement not detection plugin

---
+++ Kong/Model/BotDetectionPlugin.cs
@@ -0,0 +1,18 @@
+﻿using Kong.Serialization;
+
+namespace Kong.Model
+{
+    [Plugin("bot-detection")]
+    public class BotDetectionPlugin : PluginConfiguration
+    {
+        /// <summary>
+        /// A comma separated array of regular expressions that should be whitelisted. The regular expressions will be checked against the User-Agent header.
+        /// </summary>
+        public string[] Whilelist { get; set; }
+
+        /// <summary>
+        /// A comma separated array of regular expressions that should be blacklisted. The regular expressions will be checked against the User-Agent header.
+        /// </summary>
+        public string[] Blacklist { get; set; }
+    }
+}
