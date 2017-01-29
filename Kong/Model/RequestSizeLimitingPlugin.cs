8b385563fe735658d175681b84b8e1451d996b5c
RossJayJones <rossj@trackmatic.co.za>
2017-01-29 20:52:49 +0200

    implement request size limiting

--- Kong/Model/RequestSizeLimitingPlugin.cs
+++ Kong/Model/RequestSizeLimitingPlugin.cs
@@ -2,9 +2,15 @@
 
 namespace Kong.Model
 {
+    /// <summary>
+    /// Block incoming requests whose body is greater than a specific size in megabytes.
+    /// </summary>
     [Plugin("request-size-limiting")]
     public class RequestSizeLimitingPlugin : PluginConfiguration
     {
+        /// <summary>
+        /// Allowed request payload size in megabytes, default is 128 (128000000 Bytes)
+        /// </summary>
         public long AllowedPayloadSize { get; set; }
     }
 }
